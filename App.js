a280b725c5362afdf55f4dbb9d9c78176a4bf68d
Nicholas Gibson <Nwgibson85@gmail.com>
2019-08-22 12:01:36 -0500

    added app and index

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import Tabs from './accordian/Tabs';
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Accodian and Tabs</h1>
+        <Tabs tabs={tabsProp} />
+      </div>
+    );
+  }
+}
+
+export default App;
